a6a5ca32b4d80829ddbaa375ee43c43a5775ac01
Emma Nielsen <Emmanielsen843@gmail.com>
2023-01-05 14:37:19 +0100

    react working with sass

---
+++ spirit/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.scss';
+import { Footer } from './components/partials/footer/footer';
+import { Header } from './components/partials/header/header';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Header pageTitle="Velkommen til min react side"/>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
