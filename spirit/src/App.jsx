d7f683c909e6fdd453e2c4f4c5126e38d1a3c8ef
Emma Nielsen <Emmanielsen843@gmail.com>
2023-01-05 15:11:45 +0100

    deleted nav import

--- spirit/src/App.jsx
+++ spirit/src/App.jsx
@@ -2,7 +2,6 @@ import './App.scss';
 import { Footer } from './components/partials/footer/footer';
 import { Header } from './components/partials/header/header';
 import { MainContent } from './components/partials/main/main';
-// import { ShowNav } from './components/partials/nav/nav';
 
 
 const App = () => {
