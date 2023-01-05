0cbc9c44e9304c894ad219ca45ac539a56050f66
Emma Nielsen <Emmanielsen843@gmail.com>
2023-01-05 15:16:07 +0100

    changes

--- spirit/src/App.jsx
+++ spirit/src/App.jsx
@@ -2,13 +2,19 @@ import './App.scss';
 import { Footer } from './components/partials/footer/footer';
 import { Header } from './components/partials/header/header';
 import { MainContent } from './components/partials/main/main';
+import { ShowNav } from './components/partials/nav/nav';
 
 
 const App = () => {
   return (
     <div className="App">
       <Header pageTitle="Spirit"/>
+      <div>
+        <nav className="main-navigation">
+            <ShowNav />
+            </nav>
       <MainContent />
+      </div>
       <Footer />
     </div>
   );
--- spirit/src/components/partials/header/header.jsx
+++ spirit/src/components/partials/header/header.jsx
@@ -1,13 +1,10 @@
-import { ShowNav } from "../nav/nav"
+
 
 
 const Header = props => {
     return (
         <header className="main-header">
             <h1>{props.pageTitle}</h1>
-            <nav className="main-navigation">
-            <ShowNav />
-            </nav>
         </header>
     )
 }
